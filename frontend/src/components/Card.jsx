e2ce33022df5c836023f14097e1d601692e4fa47
Mike Jones <mikejones@yahoo.com>
2024-05-22 15:31:49 -0400

    added info to cards, mapped cards to home page

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,18 +1,27 @@
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-function Pokeitem() {
+function Pokeitem(props) {
+const {itemName,popularity,durability,price,quantity,region} = props
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
+        <Card.Title>{itemName}</Card.Title>
+        {/* <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        </Card.Text> */}
+        <ListGroup className="list-group-flush">
+            <ListGroup.Item>Popularity: {popularity}</ListGroup.Item>
+            <ListGroup.Item>Durability: {durability}</ListGroup.Item>
+            <ListGroup.Item>Price: {price}</ListGroup.Item>
+            <ListGroup.Item>Quantity: {quantity}</ListGroup.Item>
+            <ListGroup.Item>Region: {region}</ListGroup.Item>
+        </ListGroup>
+        <Button variant="primary">Add to Cart</Button>
       </Card.Body>
     </Card>
   );
